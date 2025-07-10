7eda5c09d34a5e2edbdf731cfe5e2f04986f17c6
Dushant Pariskar <dushant.pariskar@example.com>
2025-07-10 21:24:17 +0530

    message

---
+++ app.js
@@ -0,0 +1,17 @@
+
+/*
+const math = require('./math'); // assuming math.js is in the same folder
+const a = math.add(3, 4);
+console.log(a);  // Output: 7
+*/
+
+/*
+a=3
+if(a>2){
+    console.log("a is greater than 2");
+}
+else{
+    console.log("a is not greater than 2");
+}
+
+*/
---
+++ math.js
@@ -0,0 +1,8 @@
+function add(x, y) {
+  return x + y;
+}
+
+// Export the function
+module.exports = {
+  add
+};
