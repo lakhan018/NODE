1cbc5a48d5d079855dd2f3b221dcd5b70a4522f1
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-15 14:35:10 +0530

    done

---
+++ CoreModule/custom.js
@@ -0,0 +1,5 @@
+var x=require("unique69");
+var y=require("factorial");
+// console.log(x.add(3,4));
+
+console.log(y.fac(4));
---
+++ CoreModule/factorial/test.js
@@ -0,0 +1,5 @@
+
+var y=require("./fac");
+// console.log(x.add(3,4));
+
+console.log(y.fact(4));
