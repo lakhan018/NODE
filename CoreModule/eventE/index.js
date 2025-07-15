588d027d3989e7cd5bc7e3f9f8400bf51475ebf7
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-15 14:51:40 +0530

    lec

---
+++ CoreModule/eventE/index.js
@@ -0,0 +1,12 @@
+var eventemitter=require("events");
+var obj=new eventemitter();
+obj.addListener("marketOpen",(msg)=>{console.log(msg)});
+obj.addListener("marketOpen",(msg)=>{console.log(msg)});
+obj.addListener("WEATHER",(msg)=>{console.log(msg)});
+obj.emit("marketOpen","helll ow");
+obj.emit("Market","helll ow");
+obj.emit("WEATHER",2);
+obj.emit("WEATHER",1);
+
+
+//
