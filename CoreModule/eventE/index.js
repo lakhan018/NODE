207e101a63f27f6cb281306b618c5d7491d54e0b
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-15 14:54:35 +0530

    changes in event once is added here

--- CoreModule/eventE/index.js
+++ CoreModule/eventE/index.js
@@ -2,11 +2,16 @@ var eventemitter=require("events");
 var obj=new eventemitter();
 obj.addListener("marketOpen",(msg)=>{console.log(msg)});
 obj.addListener("marketOpen",(msg)=>{console.log(msg)});
-obj.addListener("WEATHER",(msg)=>{console.log(msg)});
+// obj.addListener("WEATHER",(msg)=>{console.log(msg)});
+
+obj.addListener("WEATHER",fun1);
 obj.emit("marketOpen","helll ow");
 obj.emit("Market","helll ow");
 obj.emit("WEATHER",2);
 obj.emit("WEATHER",1);
+
+function fun1(n){console.log(n)};
+
 
 
 //
