f33970af90703be94c3474da0d07a8e81906a779
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-17 15:58:21 +0530

    new file sys arnd

--- CoreModule/eventE/one.js
+++ CoreModule/eventE/one.js
@@ -1,5 +1,6 @@
 
 import fs from 'fs'
+import { fileURLToPath } from 'url'
 // var fs = require("fs")
 var json=`
 {"name":"lakhan",
@@ -7,11 +8,14 @@ var json=`
 "skills":["1","2",3],
 "add":{"city":"patiala"}
 }`
-console.log(typeof(json))
+// console.log(typeof(json))
 
-console.log(typeof(JSON.parse(json)))
-console.log((JSON.parse(json)))
+// console.log(typeof(JSON.parse(json)))
+// console.log((JSON.parse(json)))
 
 console.log(JSON.stringify(json))
-
-fs.readFileSync('x.json','utf8',()=>{console.log("read")})
+fs.readFileSync('x.json','utf8')
+{console.log("read")}
+// fs.readFile('x.json','utf8',()=>{console.log(fileURLToPath(x.json))})
+fs.readFile('x.json',(err,data)=>{if(err)return; else{console.log(JSON.parse(data))}})
+{console.log("read")}
