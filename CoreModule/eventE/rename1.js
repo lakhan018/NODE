1f30d34ea29a7bf35bf3a9bd072cfd76b8da5f44
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-18 10:18:57 +0530

    new changes

--- CoreModule/eventE/rename1.js
+++ CoreModule/eventE/rename1.js
@@ -1,5 +1,5 @@
 // import fs, { readdir } from "fs";
-var fs = require('fs')
+var fs = require("fs");
 // import { fileURLToPath } from "url";
 // fs.rename('x.json', 'tempData.json',
 //   (err) => {
@@ -16,20 +16,35 @@ var fs = require('fs')
 // fs.copyFileSync('rename1.js','temp.js')
 // fs.copyFile("temp.js", "rename2.js", (err) => {
 //   if (err) console.log(err);
-// });
+// z; // });
 
-
-var d='ficinv'
+var d = "ficinv";
 // fs.appendFileSync('tempData.json',d);
 // fs.appendFile('tempData.json',d,(err)=>{if(err){err}});
 
 // fs.appendFileSync('tempData.json',fs.readFileSync('./one.js','utf-8'))
 // fs.unlinkSync('tempData.json')
-
-
-
 // path module
-// import path from 'path'
-var path=require('path')
-var join=path.join(__dirname,'new')
-fs.readdir(join,(err,files)=>{if(err){console.log(err)}else{console.log("read"+files);}})
+// import path from 'path'const fs = require("fs");
+const path = require("path");
+
+const join = path.join(__dirname, "new");
+
+fs.readdir(join, (err, files) => {
+  if (err) {
+    console.log("Error reading directory:", err);
+  } else {
+    for (let i = 0; i < files.length; i++) {
+      const filePath = path.join(join, files[i]);
+
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) {
+          console.log("Error reading file:", err);
+        } else {
+          console.log(`Contents of ${files[i]}:\n`, data);
+        }
+      });
+    }
+    console.log("Reading files:", files);
+  }
+});
