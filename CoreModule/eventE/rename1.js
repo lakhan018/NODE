9638d887e8ca5972821e41c0657a4653979db128
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-18 10:02:30 +0530

    changes

--- CoreModule/eventE/rename1.js
+++ CoreModule/eventE/rename1.js
@@ -1,4 +1,6 @@
-import fs from 'fs';
+// import fs, { readdir } from "fs";
+var fs = require('fs')
+// import { fileURLToPath } from "url";
 // fs.rename('x.json', 'tempData.json',
 //   (err) => {
 //     if (err) {
@@ -8,6 +10,26 @@ import fs from 'fs';
 //     }
 //   });
 //  delete cmd-> unlink
-fs.unlinkSync('d.txt')
+// fs.unlinkSync('d.txt')
 // fs.unlink('deletefile',(err)=>{if else loop})
 
+// fs.copyFileSync('rename1.js','temp.js')
+// fs.copyFile("temp.js", "rename2.js", (err) => {
+//   if (err) console.log(err);
+// });
+
+
+var d='ficinv'
+// fs.appendFileSync('tempData.json',d);
+// fs.appendFile('tempData.json',d,(err)=>{if(err){err}});
+
+// fs.appendFileSync('tempData.json',fs.readFileSync('./one.js','utf-8'))
+// fs.unlinkSync('tempData.json')
+
+
+
+// path module
+// import path from 'path'
+var path=require('path')
+var join=path.join(__dirname,'new')
+fs.readdir(join,(err,files)=>{if(err){console.log(err)}else{console.log("read"+files);}})
