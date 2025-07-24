1e0ea1e7ff7ccdd4d0f67dcc423e7bd4feaa6999
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-24 15:17:19 +0530

    new work

--- CoreModule/server1.js
+++ CoreModule/server1.js
@@ -3,25 +3,27 @@
 // question ->  asume you have to create a server using http / express to fetch messge in browser
 // require is a fucntion
 // var let cosnt (var -> gloabal scope (old) let -> local scope (new))
-var http=require("http");
+var http = require("http");
 // http is module
 // http.createServer(req,res);
-http.createServer((req,res)=>{
-    if(req.url=='/home'){
-        res.write("home")
-        console.log("home")
-    // res.end();
-} else if(req.url=='/end'){
-        res.write("home")
-        console.log("home")
-    // res.end();
+http
+  .createServer((req, res) => {
+    if (req.url == "/home") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write("home");
+      console.log("home");
+      res.end();
+    } else if (req.url.startsWith("/end") ){
+      res.write("end");
+      console.log("end");
+      res.end();
+    } else {
+      res.write("hello node js");
+      console.log("no page");
+      res.end();
     }
-    else{
-res.write("hello node js")
-console.log("no page")
-res.end()
-}
-}).listen(3000);
+  })
+  .listen(3000);
 
 // while setting a cookie we uses req
 // when have to display a msg we uses response
